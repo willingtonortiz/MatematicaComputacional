2c2a952bd0733c710a18dd3fd34b7f76ba1300df
Joaquin08 <jgarciag8274@gmail.com>
2018-10-29 20:59:10 -0500

    se agrego un nuevo caracter al diccionario del AES que es '♥' , y se agrego AES al CEncriptador

--- src/app/Clases/Encriptador/CEncriptador.ts
+++ src/app/Clases/Encriptador/CEncriptador.ts
@@ -1,5 +1,19 @@
+import { AES } from '../AES/AES'
 export class CEncriptador {
     constructor() {
 
+    }
+    private encriptar(texto: string, contrasenia: string) {
+        //el nuevo caracter que agregue es ♥ 
+        let encripadorAES = new AES(texto, contrasenia, true);
+        texto=encripadorAES.textoModificado;
+        //seguir encriptando
+        return texto;
+    }
+    private desencriptar(texto: string, contrasenia: string){
+        //desencriptación del texto por los otros algoritmos
+        let desencripadorAES = new AES(texto, contrasenia, false);
+        texto=desencripadorAES.textoModificado;
+        return texto;
     }
 }
